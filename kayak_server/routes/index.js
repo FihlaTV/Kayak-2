95325429e5dbe4b79e59faad4bdb997e9dad633d
Nikita Sengupta <31235983+Nikita1409@users.noreply.github.com>
2017-11-21 00:13:52 -0800

    api to insert data into hotels

--- kayak_server/routes/index.js
+++ kayak_server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +8,39 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+router.post('/insertHotel', function (req,res,next) {
+    try{
+          var reqObj = req.body;
+          console.log(reqObj);
+          
+          req.getConnection(function (err,conn) {
+              if(err){
+                console.error('SQL connection error:', err)
+              }
+              else {
+                  var insertSql = "INSERT INTO Hotels SET ?";
+                  var insertValues = {
+                      "HotelName" : reqObj.hName,
+                      "HotelCity" : reqObj.city,
+                      "HotelAddress" : reqObj.address,
+                      "PricePerNight" : reqObj.price,
+                      "NumberSleepsPerRoom" : reqObj. sleeps,
+                      "RoomsAvailable" : reqObj.rooms,
+                      "TotalCapacity" : reqObj.capacity
+                  };
+                  var query = conn.query(insertSql,insertValues,function (err,result) {
+                      if(err){
+                        console.error('SQL error: ', err);
+                      }
+                      console.log(result);
+                      var hotel_id = result.insertId;
+                      res.json({"HotelId" : hotel_id});
+                  });
+              }
+          });
+    }
+    catch (ex){
+      console.error("internal error:"+ex);
+      return next(ex);
+    }
+});
