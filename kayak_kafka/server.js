aeab4d75ced1360eb0c06e3b86859313be2bad13
Nikita Sengupta <31235983+Nikita1409@users.noreply.github.com>
2017-11-23 01:06:23 -0800

    call to kafka queue for inserting hotels

--- kayak_kafka/server.js
+++ kayak_kafka/server.js
@@ -1,11 +1,16 @@
 var connection =  new require('./kafka/Connection');
 var addFlight = require('./services/addFlight');
 var searchFlight = require('./services/searchFlight');
+var insertHotel = require('./services/insertHotel');
 
+var topic_name1 = 'insert_hotel_topic' ;
 var topic_addFlight = 'addFlight_topic';
 var topic_searchFlight= 'searchFlight_topic';
+
+var consumer1 = connection.getConsumer(topic_name1);
 var consumer_addFlight = connection.getConsumer(topic_addFlight);
 var consumer_searchFlight = connection.getConsumer(topic_searchFlight);
+
 var producer = connection.getProducer();
 
 console.log('server is running');
@@ -48,6 +53,30 @@ consumer_searchFlight.on('message', function (message) {
         ];
         producer.send(payloads, function(err, data){
             console.log("Data"+data);
+        });
+        return;
+    });
+});
+
+consumer1.on('message', function (message) {
+    console.log('message received');
+    console.log(JSON.stringify(message.value));
+    var data = JSON.parse(message.value);
+
+    insertHotel.handle_request(data.data, function (err,res) {
+        console.log('after handle'+ res);
+        var payloads = [
+            {
+              topic : data.replyTo,
+                messages : JSON.stringify({
+                    correlationID : data.correlationID,
+                    data : res
+                }),
+                partition : 0
+            }
+        ];
+        producer.send(payloads, function (err, data) {
+           console.log(data);
         });
         return;
     });
